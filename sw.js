844e9cd751df5dfc5654501eacc29014e6626253
saidthefox <35787023+saidthefox@users.noreply.github.com>
2025-10-02 21:50:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  clients.claim();
-});
-
-self.addEventListener('fetch', event => {
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => self.clients.claim());
+self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
